fc67b2e03180bf239c09a8cfeba36fcc48f258a3
Neeraj-1007 <neerajkumarjha10@gmail.com>
2024-03-03 17:10:17 +0530

    Docker File set up

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM openjdk:8
+FROM openjdk:11
 EXPOSE 8080
 ARG JAR_FILE=target/*.jar
 COPY ${JAR_FILE} successCreations-0.0.1-SNAPSHOT.jar
--- src/main/java/com/successcreations/RootApplication.java
+++ src/main/java/com/successcreations/RootApplication.java
@@ -1,4 +1,4 @@
-package com.vastracart;
+package com.successcreations;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
